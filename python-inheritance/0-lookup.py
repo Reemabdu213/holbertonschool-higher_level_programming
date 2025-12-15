e36145eaa122c5412f698f7293857631ab944957
Reemabdu213 <11972@holbertonstudents.com>
2025-12-15 18:46:22 +0300

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -5,10 +5,10 @@
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
-    
+
     Args:
         obj: The object to inspect
-    
+
     Returns:
         A list of strings containing all attributes and methods
     """
