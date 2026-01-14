462d2e01b48c30cd44eefa5b117252989419ca65
Reemabdu213 <11972@holbertonstudents.com>
2026-01-14 18:52:17 +0300

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Create table first_table if it doesn't exist
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+-- Show full description of first_table
+SHOW CREATE TABLE first_table;
