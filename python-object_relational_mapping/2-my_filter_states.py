221b520420b497c5ac637a472a42725412d6b7eb
Reemabdu213 <11972@holbertonstudents.com>
2026-01-16 22:43:15 +0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that displays all values in states table where name matches argument"""
+"""Script that displays all values in states table where name matches
+argument"""
 
 if __name__ == "__main__":
     import MySQLdb
